cf3bf2ac0dc414c586a0a6db330856b155ed46bf
ВашÐweakaura <weakaura1@gmail.com>
2026-02-14 01:06:29 +0500

    asd

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware # Импортируем прослойку CORS
+import httpx
 
-from api.fetch import get_data
+from fetch import get_data
 
 app = FastAPI()
 
@@ -24,3 +25,8 @@ async def kaspi_data(category: Optional[str] = None):
     if category is None: return None
     return await get_data(category)
 
+@app.get("/check-ip")
+async def check_ip():
+    async with httpx.AsyncClient() as client:
+        res = await client.get("https://api.ipify.org?format=json")
+        return res.json()
